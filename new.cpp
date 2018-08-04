a9d3679b44c9f5a2c5dee25e3380a83e621c0d7c
zhzq <zhzq9009@163.com>
2018-08-04 22:58:11 +0800

    hehe

--- new.cpp
+++ new.cpp
@@ -1,1 +1,11 @@
 haha
+
+
+
+git reset HEAD~2 // 日志回退2个版本，但代码未变
+
+git checkout -- <文件或目录> //放弃当前的数据改动
+
+git reset --hard HEAD~2 //日志和代码均回退2个版本
+
+git reset --hard HEAD~2   ==  git reset HEAD~2 +  git checkout -- <文件或目录>
